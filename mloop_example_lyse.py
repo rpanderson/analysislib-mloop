7a20e8dc672851b9b0d7a1fcac997c1cf68e5b4d
rpanderson <rpanderson@localhost>
2019-05-02 12:29:21 +1000

    Skeletal version of pythonlib/mloop-optimiser:   * Runs as mutli-shot analysis script using a counter to keep track of how many     times the routine has run.   * M-LOOP optimisation runs in a threading.Thread.   * Routine counter can be passed via lyse.routine_storage or a queue.   * Multi-shot routine runs asycnrhonously with the mloop controller, but the     latter waits until the routine has run afresh before doing the next mloop     iteration.   * When the mloop controller completes the optimisation, the thread dies,     which is detected in the next run, and a new optimisation thread is started.   * mloop_example_lyse.py cannot be changed during a lyse session once it is an     active analysis routine, as labscript_utils.modulewatcher reloads modules and     this wreaks havoc presently.
    USAGE:
    Load mloop_example_lyse.py as a multi-shot routine
    
    Changes to mloop-optimiser:
       * This is only the essential bits of optimiser_interface, without any communication
         to the experiment_ or analyser_interfaces.
       * Stripped back the config reading/writing:
          - Only explicitly mloop settings are in the MLOOP section.
          - Moved opt_param and ignore_nans to a new section named ANALYSIS.
          - Removed template stuff as this won't be needed with runmanager UI scripting.
          - Fixed some path issues with where it reads/writes mloop_config.ini from/to.
    
    TODO:
      * Add more than a null-case, i.e. a successful optimisation to spoof.
      * Have this optimisation use the lyse DataFrame (will require shots to be added
        to the queue).
      * Add some type of visualisation.
      * Formalise the notion of an optimimsation session.

---
+++ mloop_example_lyse.py
@@ -0,0 +1,103 @@
+from mloop.interfaces import Interface
+from mloop.controllers import create_controller
+
+import lyse
+import time
+import threading
+
+USE_QUEUE = False
+if USE_QUEUE:
+    import Queue
+
+class LoopInterface(Interface):
+    # global lyse; import lyse
+    global config_get; import config_get
+
+    # Initialization of the interface, including this method is optional
+    def __init__(self):
+
+        # inherit parent class methods
+        super().__init__()
+
+        # Retrieve config file parameters from local file or generates and save defaults
+        self.cfg_dict = config_get.cfgget()
+
+        # initialise iteration counter
+        self.cfg_dict['iter_count'] = 0
+
+    # this is the method called by MLOOP upon each new iteration when it wants to know the cost
+    # associated with a given point in the search space
+    def get_next_cost_dict(self, params_dict):
+
+        # iterate counter and retrieve new experiment parameters
+        self.cfg_dict['iter_count'] += 1
+        self.cfg_dict['mloop_params'] = params_dict['params']
+
+        # Only proceed once per execution of the lyse routine
+        while self.cfg_dict['iter_count'] > (
+            getattr(lyse.routine_storage, "counter", 0)
+            if not USE_QUEUE
+            else lyse.routine_storage.my_queue.get()
+        ):
+            print('Waiting for next execution of lyse analysis routine...')
+            time.sleep(2)
+
+        # Return cost dictionary to M-LOOP
+        print('M-LOOP iteration  {:3d}'.format(self.cfg_dict['iter_count']))
+        cost = 1.2
+        cost_dict = {
+            'cost': float(cost),
+            'uncer': float(cost * 0.05),
+            'bad': self.cfg_dict["bad"],
+        }
+
+        return cost_dict
+
+
+def optmimus():
+    # create M-LOOP optmiser interface with desired parameters
+    opt_interface = LoopInterface()
+
+    # retrieve a snapshot of the configuration dictionary
+    opt_dict = opt_interface.cfg_dict
+
+    # instantiate experiment controller
+    controller = create_controller(opt_interface, **opt_dict)
+
+    # run the optimiser using the constructed interface
+    controller.optimize()
+
+    # The results of the optimization will be saved to files and can also be
+    # accessed as attributes of the controller.
+    print('Optimisation ended.')
+
+    # Format the results
+    opt_results = {}
+    opt_results['best_params'] = controller.best_params
+    opt_results['best_cost'] = controller.best_cost
+    opt_results['best_uncer'] = controller.best_uncer
+    opt_results['best_index'] = controller.best_index
+    return opt_results
+
+
+# Runs each time analysis routine does
+if (
+    hasattr(lyse.routine_storage, "counter")
+    and lyse.routine_storage.optimisation_thread.is_alive()
+):
+    lyse.routine_storage.counter += 1
+    print("Routine iteration {:3d}".format(lyse.routine_storage.counter))
+    if USE_QUEUE:
+        lyse.routine_storage.my_queue.put(lyse.routine_storage.counter)
+else:
+    if not hasattr(lyse.routine_storage, "counter"):
+        print("First execution of lyse routine...")
+    else:
+        print("Restarting optimisation thread...")
+    if USE_QUEUE:
+        lyse.routine_storage.my_queue = Queue.Queue()
+    lyse.routine_storage.counter = 0
+    lyse.routine_storage.optimisation_thread = threading.Thread(target=optmimus)
+    lyse.routine_storage.optimisation_thread.daemon = True
+    lyse.routine_storage.optimisation_thread.start()
+    print('Started optimisation thread...')
