20d47497861b9456c28581b16a8eccc4c40895df
rpanderson <rpanderson@localhost>
2019-05-03 20:33:13 +1000

    Changes to mloop_experiment_interface.py: * Removed redundant imports. * Some variable and function renames. * Output file name read from filename_prefix_format in mloop_config, with   default output path following prior convention:   %Y-%m-%d_{iter_count:05d}_{template_basename}   ... and uses tempalte_folder for shot_output_dir. * If filename_prefix_format is omitted from mloop_config, it and output_folder_format   are read from labconfig instead. Use this if you want sequence index to be used   to make shot_output_dir. * Needed to modify config_get.py as it was json.loads everything! This doesn't   work for the format string used in filename_prefix_format.

--- mloop_experiment_interface.py
+++ mloop_experiment_interface.py
@@ -1,83 +1,98 @@
 import lyse
 import os
 import zmq
-import h5py
-import time
 import signal
 import datetime
 import zprocess
-import numpy as np
 import runmanager as rm
-import labscript_utils.labconfig as labconfig
-import labscript_utils.shared_drive as shared_drive
+from labscript_utils import labconfig
+from labscript_utils import shared_drive
 
-#=========================================================================
+# =========================================================================
 
 # Lab configuration
 lc = labconfig.LabConfig()
+shot_storage = lc.get('DEFAULT', 'experiment_shot_storage')
+blacs_port = lc.getint('ports', 'blacs')
+blacs_host = lc.get('servers', 'blacs')
+mloop_port = lc.getint('ports', 'compiler')
+mloop_timeout = lc.getint('DEFAULT', 'server_timeout', fallback=5)
 
-# Path to experiment shot storage
-experiment_shot_storage = lc.get('DEFAULT', 'experiment_shot_storage')
+# =========================================================================
 
-# server wait time
-server_timeout = int(lc.get('DEFAULT', 'server_timeout'))
+def compile_and_run_shot(config):
 
-# Ports
-BLACS_port = int(lc.get('ports', 'blacs'))
-BLACS_hostname = lc.get('servers', 'blacs')
-mloop_experiment_port = int(lc.get('ports', 'mloop-experiment'))
-
-#=========================================================================
-
-# mloop experiment headless run manager
-def mloop_run_experiment(config_dict):
-
-    # Copy hdf5 file ready for optimization
+    # Copy hdf5 file ready for optimisation
     now = datetime.datetime.now()
 
     # extract relevant information from master config file
-    params_to_change = config_dict["params_to_change"]
-    template_file = config_dict["template_file"]
-    template_folder = config_dict["template_folder"]
-    globals_groups = config_dict["globals_groups"]
-    labscript_file = config_dict["labscript_file"]
-    globals_values = config_dict["mloop_params"]
+    params_to_change = config['params_to_change']
+    template_file = config['template_file']
+    template_folder = config['template_folder']
+    globals_groups = config['globals_groups']
+    labscript_file = config['labscript_file']
+    globals_values = config['mloop_params']
 
-    # Setup paths
+    # Path to single (template) shot file whose expanded globals to compile with
     template_path = os.path.join(template_folder, template_file)
 
-    labscriptlib_path = lc.get('DEFAULT', 'labscriptlib')
-    labscript_path = os.path.join(labscriptlib_path, labscript_file)
+    # Path to experiment script
+    labscript_path = os.path.join(lc.get('DEFAULT', 'labscriptlib'), labscript_file)
 
     # TODO: Change output_file, sequence_index, run number attribute, mloop session id, in h5 file also
-    output_file = template_file.replace('.h5', '_{}_mloop{:05d}.h5'.format(datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S"), config_dict["iter_count"]))
-    output_path = os.path.join(template_folder, output_file)
+    try:
+        shot_output_dir = template_folder
+        filename_prefix = now.strftime(config['filename_prefix_format']).format(
+            template_basename=os.path.splitext(template_file)[0],
+            iter_count=config['iter_count']
+            )
+    except KeyError:
+        sequence_attrs, shot_output_dir, filename_prefix = rm.new_sequence_details(
+            script_path=labscript_path, 
+            config=lc, 
+            increment_sequence_index=False
+            )
+
+    output_file = filename_prefix + '.h5'
+    output_path = os.path.join(shot_output_dir, output_file) 
+    print(output_path)
 
     # Change optimisation globals in place
     for i in range(len(params_to_change)):
-        print('Changing value of {:}/{:} to {:}'.format(globals_groups[i], params_to_change[i], globals_values[i]))
-        rm.set_value(template_path, globals_groups[i], params_to_change[i], globals_values[i])
+        print(
+            '   Changing value of {:}/{:} to {:}'.format(
+                globals_groups[i], params_to_change[i], globals_values[i]
+            )
+        )
+        rm.set_value(
+            template_path, globals_groups[i], params_to_change[i], globals_values[i]
+        )
 
-    # Run shot of the experiment via BLACS
-    rm.compile_labscript_with_globals_files_async(labscript_path, template_path, output_path,
-                                                  0,
-                                                  # lambda x: print('Experiment submitted! (Not.)'))
-                                                  lambda x: zprocess.zmq_get(BLACS_port, BLACS_hostname,
-                                                                             data=shared_drive.path_to_agnostic(output_path)))
+    # Compile shot and immediately submit to BLACS
+    rm.compile_labscript_with_globals_files_async(
+        labscript_file=labscript_path,
+        globals_files=template_path,
+        output_path=output_path,
+        stream_port=0,
+        done_callback=lambda x: zprocess.zmq_get(
+            blacs_port, blacs_host, data=shared_drive.path_to_agnostic(output_path)
+        ),
+    )
 
-if __name__ == "__main__":
 
-    print("Opening ZProcess Server for Experiment\n")
+if __name__ == '__main__':
+
+    print('Launching zprocess server for experiment compilation and submission.\n\n')
 
     # setup port listening with default time out (estimated single shot max time)
     context = zmq.Context()
     socket = context.socket(zmq.REP)
     socket.setsockopt(zmq.LINGER, 0)
-    socket.RCVTIMEO = server_timeout*1000
-    socket.bind("tcp://0.0.0.0:{}".format(mloop_experiment_port))
-    
+    socket.RCVTIMEO = mloop_timeout * 1000
+    socket.bind('tcp://0.0.0.0:{}'.format(mloop_port))
+
     def exit_handler(*args, **kwargs):
-        # close socket 
+        # close socket
         socket.close()
         # terminate context
         context.term()
@@ -91,11 +106,12 @@ if __name__ == "__main__":
         try:
             # recieve configuration dictionary with updated parameters and block
             # until recieved or timeout
-            config_dict = socket.recv_pyobj()
+            print('\nWaiting for compilation request...')
+            config = socket.recv_pyobj()
         except zmq.error.Again:
-            print('\nExperiment interface timed out!')
+            print('\nTimed out waiting for updated parameters.')
             exit_handler()
 
-        print("\nRunning new experiment with specified parameters")
-        mloop_run_experiment(config_dict)
-        socket.send_pyobj("Experiment parameters recieved", protocol=2)
+        print('Received updated parameters...')
+        compile_and_run_shot(config)
+        socket.send_pyobj('Experiment parameters recieved', protocol=2)
