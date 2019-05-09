c3f27e2ac6f230cc238cc9672c6a1df2f00bf616
Russell Anderson <5637107+rpanderson@users.noreply.github.com>
2019-05-09 22:49:41 +1000

    Refactor: * Omit redundant items in mloop_config.ini and renamed opt_param to cost_key * Added small delay between setting mloop_iteration and rm.remote.engage as   I was seeing some shots without an updated value of this global. * Removed extraneous variables throughout. * cost_analysis now returns an M-LOOP cost_dict so that it rather than just the   cost value are put/get in/from the lyse.routine_storage.queue. * Retired ignore_nans (always report bad shot if cost is nan or inf). * plot_mloop_results.py reads the config file to determine DataFrame columns.

---
+++ mloop_config.py
@@ -0,0 +1,97 @@
+import os
+import sys
+import json
+import configparser
+
+
+def get(config_path=None):
+    """Creates config file from specified file, or 
+    creates one locally with default values.
+    """
+
+    # Default to local directory and default name
+    if not config_path:
+        folder = os.path.dirname(__file__)
+        config_path = os.path.join(folder, "mloop_config.ini")
+
+    # Instantiate RawConfigParser with case sensitive option names
+    config = configparser.RawConfigParser()
+    config.optionxform = str
+
+    # Check if file exists and initialise with defaults if it does not
+    if os.path.isfile(config_path):
+        # Retrieve configuration parameters
+        config.read(config_path)
+    else:
+        print("--- Configuration file not found: generating with default values ---")
+
+        # Shot compilation parameters
+        config["COMPILATION"] = {}
+        config["COMPILATION"]["mock"] = 'false'
+
+        # Analayis parameters
+        config["ANALYSIS"] = {}
+        # lyse DataFrame key to optimise
+        config["ANALYSIS"]["cost_key"] = '["fake_result", "y"]'
+        # Maximize cost_key (negate when reporting cost)
+        config["ANALYSIS"]["maximize"] = 'true'
+
+        # M-LOOP parameters
+        config["MLOOP"] = {}
+        # Parameters mloop varies during optimisation
+        config["MLOOP"][
+            "mloop_params"
+        ] = '{"x": {"min": -5.0, "max": 5.0, "start": -2.0} }'
+        # Number of training runs
+        config["MLOOP"]["num_training_runs"] = '5'
+        # Maximum number of iterations
+        config["MLOOP"]["max_num_runs_without_better_params"] = '10'
+        # Maximum number of iterations
+        config["MLOOP"]["max_num_runs"] = '20'
+        # Maximum % move distance from best params
+        config["MLOOP"]["trust_region"] = '0.5'
+        # Maximum number of iterations
+        config["MLOOP"]["cost_has_noise"] = 'true'
+        # Force mloop to return a parameter prediction before it is ready
+        config["MLOOP"]["no_delay"] = 'false'
+        # Display visualisations
+        config["MLOOP"]["visualisations"] = 'false'
+        # Type of learner to use in optimisation: [gaussian_process, random, nelder_mead]
+        config["MLOOP"]["controller_type"] = '"gaussian_process"'
+        # Mute output from MLOOP optimiser
+        config["MLOOP"]["console_log_level"] = '"NOTSET"'
+
+        # Write to file
+        folder = os.path.dirname(__file__)
+        with open(os.path.join(folder, "mloop_config.ini"), "w+") as f:
+            config.write(f)
+
+    # iterate over configuration object and store pairs in parameter dictionary
+    params = {}
+    for sect in config.sections():
+        for (key, val) in config.items(sect):
+            try:
+                params[key] = json.loads(val)
+            except json.JSONDecodeError:
+                params[key] = val
+
+    # Convert cost_key to tuple
+    params["cost_key"] = tuple(params["cost_key"])
+
+    # store number of parameters for passing to controller interface
+    params["num_params"] = len(params["mloop_params"])
+
+    # get min boundaries for specified variables
+    params["min_boundary"] = [param["min"] for param in params["mloop_params"].values()]
+
+    # get max boundaries for specified variables
+    params["max_boundary"] = [param["max"] for param in params["mloop_params"].values()]
+
+    # starting point for search space, default to half point if not defined
+    params["first_params"] = [param["start"] for param in params["mloop_params"].values()]
+
+    return params
+
+
+if __name__ == "__main__":
+    print(get())
