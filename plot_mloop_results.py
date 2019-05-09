5c847b4c072a838d751fe4c97cbd599216e570f4
rpanderson <rpanderson@localhost>
2019-05-09 16:51:15 +1000

    * Depend on runmanager.remote and thus remove:    - all reference to the template shot file, experiment script, globals groups    - mloop_experiment_interface.py    - autogenerate_template section * Add setting of globals 'mloop_session' and 'mloop_iteration' if they exist,   as an ad hoc way of keeping track of optimisation sessions in lieu of a   sub-sequence index and/or new sequence attrs. * Adding neglected fake_result.py used to generate optimisation parameter when   testing. * Updated plot_mloop_results.py to use mloop_session if available.

--- plot_mloop_results.py
+++ plot_mloop_results.py
@@ -10,10 +10,19 @@ def sinc2(x, s=0.01):
 
 try:
     df = lyse.data()
-    df.plot(x='x', y=('fake_result', 'y'), kind='scatter')
+    try:
+        # Try to use the most recent mloop_session ID
+        gb = df.groupby('mloop_session')
+        mloop_session = list(gb.groups.keys())[-1]
+        subdf = gb.get_group(mloop_session)
+    except:
+        subdf = df
+        mloop_session = None
+    subdf.plot(x='x', y=('fake_result', 'y'), kind='scatter')
     x_p = np.linspace(df.x.min(), df.x.max(), 100)
     plt.plot(x_p, sinc2(x_p, s=0))
     plt.axis(ymin=0, ymax=1.1)
+    plt.title('M-LOOP session: {:}'.format(mloop_session))
     plt.show()
 except:
     pass
